a45f3f66809eabff6939aefa1a489414233f6524
Steve Mokris <smokris@softpixel.com>
2017-09-21 20:31:11 -0400

    Initial commit

---
+++ conanfile.py
@@ -0,0 +1,43 @@
+from conans import ConanFile, tools, AutoToolsBuildEnvironment
+import os
+
+class GettextConan(ConanFile):
+    name = 'gettext'
+    version = '0.19.8.1'
+    settings = 'os', 'compiler', 'build_type', 'arch'
+    url = 'https://github.com/vuo/conan-gettext'
+    license = 'https://www.gnu.org/software/gettext/manual/html_node/GNU-LGPL.html'
+    description = 'Helps other GNU packages produce multi-lingual messages'
+    source_dir = 'gettext-%s' % version
+    build_dir = '_build'
+
+    def source(self):
+        # The .xz and .lz archives are much smaller, but Conan doesn't yet support those archive formats.
+        # https://github.com/conan-io/conan/issues/52
+        tools.get('https://ftp.gnu.org/pub/gnu/gettext/gettext-%s.tar.gz' % self.version,
+                  # sha256='105556dbc5c3fbbc2aa0edb46d22d055748b6f5c7cd7a8d99f8e7eb84e938be4'
+                  )
+
+    def build(self):
+        tools.mkdir(self.build_dir)
+        with tools.chdir(self.build_dir):
+            autotools = AutoToolsBuildEnvironment(self)
+            autotools.cxx_flags.append('-Oz')
+            autotools.cxx_flags.append('-mmacosx-version-min=10.8')
+            autotools.link_flags.append('-Wl,-install_name,@rpath/libintl.dylib')
+            autotools.configure(configure_dir='../%s' % self.source_dir,
+                                args=['--quiet',
+                                      '--disable-c++',
+                                      '--disable-curses',
+                                      '--disable-java',
+                                      '--disable-static',
+                                      '--enable-shared',
+                                      '--prefix=%s' % os.getcwd()])
+            autotools.make(args=['install'])
+
+    def package(self):
+        self.copy('*.h', src='%s/include' % self.build_dir, dst='include')
+        self.copy('libintl.dylib', src='%s/lib' % self.build_dir, dst='lib')
+
+    def package_info(self):
+        self.cpp_info.libs = ['intl']
---
+++ test_package/test_package.cc
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <libintl.h>
+
+int main()
+{
+	printf("%s\n", gettext("Successfully initialized gettext."));
+	return 0;
+}
