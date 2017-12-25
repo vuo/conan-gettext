1a60d4ecfd85cbfbef4d6fe018edd8d5b5a084b1
Steve Mokris <smokris@softpixel.com>
2017-12-25 15:46:22 -0500

    Update minimum macOS version.  [#13885]

--- conanfile.py
+++ conanfile.py
@@ -3,18 +3,22 @@ import os
 
 class GettextConan(ConanFile):
     name = 'gettext'
-    version = '0.19.8.1'
+
+    source_version = '0.19.8.1'
+    package_version = '2'
+    version = '%s-%s' % (source_version, package_version)
+
     settings = 'os', 'compiler', 'build_type', 'arch'
     url = 'https://github.com/vuo/conan-gettext'
     license = 'https://www.gnu.org/software/gettext/manual/html_node/GNU-LGPL.html'
     description = 'Helps other GNU packages produce multi-lingual messages'
-    source_dir = 'gettext-%s' % version
+    source_dir = 'gettext-%s' % source_version
     build_dir = '_build'
 
     def source(self):
         # The .xz and .lz archives are much smaller, but Conan doesn't yet support those archive formats.
         # https://github.com/conan-io/conan/issues/52
-        tools.get('https://ftp.gnu.org/pub/gnu/gettext/gettext-%s.tar.gz' % self.version,
+        tools.get('https://ftp.gnu.org/pub/gnu/gettext/gettext-%s.tar.gz' % self.source_version,
                   sha256='ff942af0e438ced4a8b0ea4b0b6e0d6d657157c5e2364de57baa279c1c125c43')
 
     def build(self):
@@ -22,7 +26,7 @@ class GettextConan(ConanFile):
         with tools.chdir(self.build_dir):
             autotools = AutoToolsBuildEnvironment(self)
             autotools.cxx_flags.append('-Oz')
-            autotools.cxx_flags.append('-mmacosx-version-min=10.8')
+            autotools.cxx_flags.append('-mmacosx-version-min=10.10')
             autotools.link_flags.append('-Wl,-install_name,@rpath/libintl.dylib')
             autotools.configure(configure_dir='../%s' % self.source_dir,
                                 args=['--quiet',
