56f8e0056f322eaee23488d82b8c06755e58d478
smokris <smokris@softpixel.com>
2018-04-02 18:54:31 -0400

    Include the license in the package.

--- conanfile.py
+++ conanfile.py
@@ -22,6 +22,8 @@ class GettextConan(ConanFile):
         tools.get('https://ftp.gnu.org/pub/gnu/gettext/gettext-%s.tar.gz' % self.source_version,
                   sha256='ff942af0e438ced4a8b0ea4b0b6e0d6d657157c5e2364de57baa279c1c125c43')
 
+        self.run('mv %s/gettext-runtime/intl/COPYING.LIB %s/libintl.txt' % (self.source_dir, self.source_dir, self.name))
+
     def build(self):
         tools.mkdir(self.build_dir)
         with tools.chdir(self.build_dir):
@@ -34,7 +36,6 @@ class GettextConan(ConanFile):
                 'CC' : self.deps_cpp_info['llvm'].rootpath + '/bin/clang',
                 'CXX': self.deps_cpp_info['llvm'].rootpath + '/bin/clang++',
             }
-
             with tools.environment_append(env_vars):
                 autotools.configure(configure_dir='../%s' % self.source_dir,
                                     args=['--quiet',
@@ -49,6 +50,8 @@ class GettextConan(ConanFile):
     def package(self):
         self.copy('*.h', src='%s/include' % self.build_dir, dst='include')
         self.copy('libintl.dylib', src='%s/lib' % self.build_dir, dst='lib')
+
+        self.copy('libintl.txt' % self.name, src=self.source_dir, dst='license')
 
     def package_info(self):
         self.cpp_info.libs = ['intl']
