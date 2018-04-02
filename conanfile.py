e03236b61c657532020114f6827ce20c2651f2db
smokris <smokris@softpixel.com>
2018-04-02 18:48:59 -0400

    Increment package version.

--- conanfile.py
+++ conanfile.py
@@ -5,7 +5,7 @@ class GettextConan(ConanFile):
     name = 'gettext'
 
     source_version = '0.19.8.1'
-    package_version = '2'
+    package_version = '3'
     version = '%s-%s' % (source_version, package_version)
 
     settings = 'os', 'compiler', 'build_type', 'arch'
