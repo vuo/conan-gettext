848724722b2d60f8e8f3b62d0da75b70a3bb3af9
Steve Mokris <steve@kosada.com>
2020-11-06 13:39:13 -0500

    Update to gettext 0.21 (latest).  Build for both x86_64 + arm64.  [kosada/vuo/vuo#17836]

--- conanfile.py
+++ conanfile.py
@@ -5,18 +5,26 @@ import platform
 class GettextConan(ConanFile):
     name = 'gettext'
 
-    source_version = '0.19.8.1'
-    package_version = '3'
+    source_version = '0.21'
+    package_version = '0'
     version = '%s-%s' % (source_version, package_version)
 
-    build_requires = 'llvm/3.3-5@vuo/stable', \
-               'vuoutils/1.0@vuo/stable'
+    build_requires = (
+        'llvm/5.0.2-1@vuo/stable',
+        'macos-sdk/11.0-0@vuo/stable',
+    )
     settings = 'os', 'compiler', 'build_type', 'arch'
     url = 'https://github.com/vuo/conan-gettext'
     license = 'https://www.gnu.org/software/gettext/manual/html_node/GNU-LGPL.html'
     description = 'Helps other GNU packages produce multi-lingual messages'
     source_dir = 'gettext-%s' % source_version
-    build_dir = '_build'
+
+    build_x86_dir = '_build_x86'
+    build_arm_dir = '_build_arm'
+    install_x86_dir = '_install_x86'
+    install_arm_dir = '_install_arm'
+    install_universal_dir = '_install_universal_dir'
+
     libs = {
         'intl': 0,
     }
@@ -28,49 +36,73 @@ class GettextConan(ConanFile):
             raise Exception('Unknown platform "%s"' % platform.system())
 
     def source(self):
-        # The .xz and .lz archives are much smaller, but Conan doesn't yet support those archive formats.
-        # https://github.com/conan-io/conan/issues/52
-        tools.get('https://ftp.gnu.org/pub/gnu/gettext/gettext-%s.tar.gz' % self.source_version,
-                  sha256='ff942af0e438ced4a8b0ea4b0b6e0d6d657157c5e2364de57baa279c1c125c43')
+        tools.get('https://ftp.gnu.org/pub/gnu/gettext/gettext-%s.tar.xz' % self.source_version,
+                  sha256='d20fcbb537e02dcf1383197ba05bd0734ef7bf5db06bdb241eb69b7d16b73192')
 
         self.run('mv %s/gettext-runtime/intl/COPYING.LIB %s/libintl.txt' % (self.source_dir, self.source_dir))
 
     def build(self):
-        import VuoUtils
-        tools.mkdir(self.build_dir)
-        with tools.chdir(self.build_dir):
-            autotools = AutoToolsBuildEnvironment(self)
+        autotools = AutoToolsBuildEnvironment(self)
 
-            # The LLVM/Clang libs get automatically added by the `requires` line,
-            # but this package doesn't need to link with them.
-            autotools.libs = ['c++abi']
+        # The LLVM/Clang libs get automatically added by the `requires` line,
+        # but this package doesn't need to link with them.
+        autotools.libs = []
 
-            autotools.flags.append('-Oz')
+        autotools.flags.append('-Oz')
 
-            if platform.system() == 'Darwin':
-                autotools.flags.append('-mmacosx-version-min=10.10')
-                autotools.link_flags.append('-Wl,-install_name,@rpath/libintl.dylib')
+        if platform.system() == 'Darwin':
+            autotools.flags.append('-isysroot %s' % self.deps_cpp_info['macos-sdk'].rootpath)
+            autotools.flags.append('-mmacosx-version-min=10.11')
+            autotools.link_flags.append('-Wl,-install_name,@rpath/libintl.dylib')
 
-            env_vars = {
-                'CC' : self.deps_cpp_info['llvm'].rootpath + '/bin/clang',
-                'CXX': self.deps_cpp_info['llvm'].rootpath + '/bin/clang++',
-            }
-            with tools.environment_append(env_vars):
+        common_configure_args = [
+            '--quiet',
+            '--disable-c++',
+            '--disable-curses',
+            '--disable-java',
+            '--disable-static',
+            '--enable-shared',
+        ]
+
+        env_vars = {
+            'CC' : self.deps_cpp_info['llvm'].rootpath + '/bin/clang',
+            'CXX': self.deps_cpp_info['llvm'].rootpath + '/bin/clang++',
+            'CFLAGS'  : '-isystem %s/usr/include' % self.deps_cpp_info['macos-sdk'].rootpath,
+            'CPPFLAGS': '-isystem %s/usr/include' % self.deps_cpp_info['macos-sdk'].rootpath,
+        }
+        with tools.environment_append(env_vars):
+            build_root = os.getcwd()
+
+            self.output.info("=== Build for x86_64 ===")
+            tools.mkdir(self.build_x86_dir)
+            with tools.chdir(self.build_x86_dir):
+                autotools.flags.append('-arch x86_64')
+                autotools.link_flags.append('-arch x86_64')
                 autotools.configure(configure_dir='../%s' % self.source_dir,
-                                    args=['--quiet',
-                                          '--disable-c++',
-                                          '--disable-curses',
-                                          '--disable-java',
-                                          '--disable-static',
-                                          '--enable-shared',
-                                          '--prefix=%s' % os.getcwd()])
-                autotools.make(args=['install'])
+                                    build=False,
+                                    host=False,
+                                    args=common_configure_args + [
+                                        '--prefix=%s/%s' % (build_root, self.install_x86_dir),
+                                    ])
+                autotools.make(args=['--quiet'])
+                autotools.make(target='install', args=['--quiet'])
 
-            if platform.system() == 'Linux':
-                with tools.chdir('lib'):
-                    self.run('mv preloadable_libintl.so libintl.so')
-                    self.run('chmod +x libintl.so')
-                    VuoUtils.fixLibs(self.libs, self.deps_cpp_info)
+            self.output.info("=== Build for arm64 ===")
+            tools.mkdir(self.build_arm_dir)
+            with tools.chdir(self.build_arm_dir):
+                autotools.flags.remove('-arch x86_64')
+                autotools.flags.append('-arch arm64')
+                autotools.link_flags.remove('-arch x86_64')
+                autotools.link_flags.append('-arch arm64')
+                autotools.configure(configure_dir='../%s' % self.source_dir,
+                                    build=False,
+                                    host=False,
+                                    args=common_configure_args + [
+                                        '--prefix=%s/%s' % (build_root, self.install_arm_dir),
+                                        '--host=x86_64-apple-darwin15.0.0',
+                                    ])
+                autotools.make(args=['--quiet'])
+                autotools.make(target='install', args=['--quiet'])
 
     def package(self):
         if platform.system() == 'Darwin':
@@ -78,8 +110,12 @@ class GettextConan(ConanFile):
         elif platform.system() == 'Linux':
             libext = 'so'
 
-        self.copy('*.h', src='%s/include' % self.build_dir, dst='include')
-        self.copy('libintl.%s' % libext, src='%s/lib' % self.build_dir, dst='lib')
+        tools.mkdir(self.install_universal_dir)
+        with tools.chdir(self.install_universal_dir):
+            self.run('lipo -create ../%s/lib/libintl.%s ../%s/lib/libintl.%s -output libintl.%s' % (self.install_x86_dir, libext, self.install_arm_dir, libext, libext))
+
+        self.copy('*.h', src='%s/include' % self.install_x86_dir, dst='include')
+        self.copy('libintl.%s' % libext, src='%s' % self.install_universal_dir, dst='lib')
 
         self.copy('libintl.txt', src=self.source_dir, dst='license')
 
